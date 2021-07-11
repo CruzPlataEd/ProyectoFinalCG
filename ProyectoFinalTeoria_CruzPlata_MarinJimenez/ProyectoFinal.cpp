5a10657eceed1777f6ffaa572977cefbc2d01c46
Luis Antonio Cruz Plata <luisantonio27@live.com.mx>
2021-07-11 15:17:42 -0500

    Inicio de proyecto final en git

---
+++ ProyectoFinalTeoria_CruzPlata_MarinJimenez/ProyectoFinal.cpp
@@ -0,0 +1,404 @@
+#include <Windows.h>
+
+#include <glad/glad.h>
+#include <glfw3.h>	//main
+#include <stdlib.h>		
+#include <glm/glm.hpp>	//camera y model
+#include <glm/gtc/matrix_transform.hpp>	//camera y model
+#include <glm/gtc/type_ptr.hpp>
+#include <time.h>
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>	//Texture
+
+#define SDL_MAIN_HANDLED
+#include <SDL/SDL.h>
+
+#include <shader_m.h>
+#include <camera.h>
+#include <modelAnim.h>
+#include <model.h>
+#include <Skybox.h>
+#include <iostream>
+
+//#pragma comment(lib, "winmm.lib")
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+//void my_input(GLFWwindow *window);
+void my_input(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// settings
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
+GLFWmonitor *monitors;
+
+void getResolution(void);
+
+// camera
+Camera camera(glm::vec3(0.0f, 10.0f, 90.0f));
+float MovementSpeed = 2.0f;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+const int FPS = 60;
+const int LOOP_TIME = 1000 / FPS; // = 16 milisec // 1000 millisec == 1 sec
+double	deltaTime = 0.0f,
+lastFrame = 0.0f;
+
+//Lighting
+glm::vec3 lightPosition(0.0f, 4.0f, -10.0f);
+glm::vec3 lightDirection(0.0f, -1.0f, -1.0f);
+
+
+float tiempo = 0.0f;
+
+
+void getResolution()
+{
+	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	SCR_WIDTH = mode->width;
+	SCR_HEIGHT = (mode->height) - 80;
+}
+
+void animate(void)
+{
+	if (tiempo >= 0.0f and tiempo < 1.0f) {
+		tiempo += 0.01f;
+	}
+	if (tiempo >= 1.0f and tiempo < 2.0f) {
+		tiempo += 0.01f;
+	}
+	if (tiempo >= 2.0f)
+		tiempo -= 2.0f;
+}
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// glfw window creation
+	// --------------------
+	// --------------------
+	monitors = glfwGetPrimaryMonitor();
+	getResolution();
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "CGeIHC", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetWindowPos(window, 0, 30);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetKeyCallback(window, my_input);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// configure global opengl state
+	// -----------------------------
+	glEnable(GL_DEPTH_TEST);
+
+	// build and compile shaders
+	// -------------------------
+	Shader staticShader("Shaders/shader_Lights.vs", "Shaders/shader_Lights.fs");
+	Shader skyboxShader("Shaders/skybox.vs", "Shaders/skybox.fs");
+	Shader animShader("Shaders/anim.vs", "Shaders/anim.fs");
+
+
+	//Aquí se colocan las imagenes que irán en el skybox
+	vector<std::string> x{};
+
+	vector<std::string> faces1
+	{
+		"resources/skybox/AtardecerRight.jpg",
+		"resources/skybox/AtardecerLeft.jpg",
+		"resources/skybox/AtardecerTop.jpg",
+		"resources/skybox/AtardecerBottom.jpg",
+		"resources/skybox/AtardecerFront.jpg",
+		"resources/skybox/AtardecerBack.jpg"
+	};
+
+	vector<std::string> faces2
+	{
+		"resources/skybox/right.jpg",
+		"resources/skybox/left.jpg",
+		"resources/skybox/top.jpg",
+		"resources/skybox/bottom.jpg",
+		"resources/skybox/front.jpg",
+		"resources/skybox/back.jpg"
+	};
+
+	
+
+	if (tiempo >= 0.0f and tiempo < 100.0f) {
+		x = faces1;
+	}
+	if (tiempo >= 100.0f and tiempo < 200.0f) {
+		x = faces2;
+	}
+
+	Skybox skybox = Skybox(x);
+
+	
+
+	// Shader configuration
+	// --------------------
+	skyboxShader.use();
+	skyboxShader.setInt("skybox", 0);
+
+	// load models
+	// -----------
+	Model piso("resources/objects/piso/piso.obj");
+	Model casaModerna("resources/objects/CasaModerna/Farm_house.obj");
+
+
+	// draw in wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		animate();
+
+		skyboxShader.setInt("skybox", 0);
+
+		// per-frame time logic
+		// --------------------
+		lastFrame = SDL_GetTicks();
+
+		// input
+		// -----
+		//my_input(window);
+
+		// render
+		// ------
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// don't forget to enable shader before setting uniforms
+		staticShader.use();
+		//Setup Advanced Lights
+		//Fuente de luz direccional (Trata de ser una fuente de luz parecida al sol)
+		staticShader.setVec3("viewPos", camera.Position);
+		staticShader.setVec3("dirLight.direction", lightDirection);
+		staticShader.setVec3("dirLight.ambient", glm::vec3(0.0f+tiempo, 0.0f+tiempo, 0.0f+tiempo));
+		staticShader.setVec3("dirLight.diffuse", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setVec3("dirLight.specular", glm::vec3(0.0f, 0.0f, 0.0f));
+
+		//Fuentes de luz posicionales (Replicar los focos, lamparas, etc)
+		staticShader.setVec3("pointLight[0].position", lightPosition);
+		staticShader.setVec3("pointLight[0].ambient", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setVec3("pointLight[0].diffuse", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setVec3("pointLight[0].specular", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setFloat("pointLight[0].constant", 0.08f);
+		staticShader.setFloat("pointLight[0].linear", 0.009f);    // La linear y la constant dicen que tanto se mueve la luz
+		staticShader.setFloat("pointLight[0].quadratic", 0.032f); // intensidad de la luz
+
+		staticShader.setVec3("pointLight[1].position", glm::vec3(-80.0, 0.0f, 0.0f));
+		staticShader.setVec3("pointLight[1].ambient", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setVec3("pointLight[1].diffuse", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setVec3("pointLight[1].specular", glm::vec3(0.0f, 0.0f, 0.0f));
+		staticShader.setFloat("pointLight[1].constant", 1.0f);
+		staticShader.setFloat("pointLight[1].linear", 0.009f);
+		staticShader.setFloat("pointLight[1].quadratic", 0.032f);
+		
+		staticShader.setFloat("material_shininess", 32.0f);
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 tmp = glm::mat4(1.0f);
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10000.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		staticShader.setMat4("projection", projection);
+		staticShader.setMat4("view", view);
+
+		//// Light
+		glm::vec3 lightColor = glm::vec3(0.6f);
+		glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
+		glm::vec3 ambientColor = diffuseColor * glm::vec3(0.75f);
+
+
+		// -------------------------------------------------------------------------------------------------------------------------
+		// Personaje Animacion
+		// -------------------------------------------------------------------------------------------------------------------------
+		//Remember to activate the shader with the animation
+		animShader.use();
+		animShader.setMat4("projection", projection);
+		animShader.setMat4("view", view);
+
+		animShader.setVec3("material.specular", glm::vec3(0.5f));
+		animShader.setFloat("material.shininess", 32.0f);
+		animShader.setVec3("light.ambient", ambientColor);
+		animShader.setVec3("light.diffuse", diffuseColor);
+		animShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		animShader.setVec3("light.direction", lightDirection);
+		animShader.setVec3("viewPos", camera.Position);
+
+
+		//Animación del personaje
+		/*model = glm::translate(glm::mat4(1.0f), glm::vec3(-40.3f, 1.75f, 0.3f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(1.2f));	// it's a bit too big for our scene, so scale it down
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		animShader.setMat4("model", model);
+		animacionPersonaje.Draw(animShader);*/
+
+		// -------------------------------------------------------------------------------------------------------------------------
+		// Segundo Personaje Animacion
+		// -------------------------------------------------------------------------------------------------------------------------
+
+		/*model = glm::translate(glm::mat4(1.0f), glm::vec3(40.3f, 1.75f, 0.3f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.5f));	// it's a bit too big for our scene, so scale it down
+		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		animShader.setMat4("model", model);
+		ninja.Draw(animShader);*/
+
+		// -------------------------------------------------------------------------------------------------------------------------
+		// Escenario
+		// -------------------------------------------------------------------------------------------------------------------------
+		staticShader.use();
+		staticShader.setMat4("projection", projection);
+		staticShader.setMat4("view", view);
+
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(100.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		staticShader.setMat4("model", model);
+		casaModerna.Draw(staticShader);
+		//casaDoll.Draw(staticShader);  Cambiamos esta linea y la remplazamos
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.2f));
+		staticShader.setMat4("model", model);
+		piso.Draw(staticShader);
+
+	
+
+		// -------------------------------------------------------------------------------------------------------------------------
+		// Caja Transparente --- Siguiente Práctica
+		// -------------------------------------------------------------------------------------------------------------------------
+		/*glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -70.0f));
+		model = glm::scale(model, glm::vec3(5.0f));
+		staticShader.setMat4("model", model);
+		nube.Draw(staticShader);
+		glEnable(GL_BLEND);*/
+		// -------------------------------------------------------------------------------------------------------------------------
+		// Termina Escenario
+		// -------------------------------------------------------------------------------------------------------------------------
+
+		//-------------------------------------------------------------------------------------
+		// draw skybox as last
+		// -------------------
+		skyboxShader.use();
+		skybox.Draw(skyboxShader, view, projection, camera);
+
+		// Limitar el framerate a 60
+		deltaTime = SDL_GetTicks() - lastFrame; // time for full 1 loop
+		//std::cout <<"frame time = " << frameTime << " milli sec"<< std::endl;
+		if (deltaTime < LOOP_TIME)
+		{
+			SDL_Delay((int)(LOOP_TIME - deltaTime));
+		}
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	skybox.Terminate();
+
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void my_input(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, (float)deltaTime);
+
+	//To Configure Model
+	if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
+		lightPosition.x++;
+	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+		lightPosition.x--;
+
+	
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
