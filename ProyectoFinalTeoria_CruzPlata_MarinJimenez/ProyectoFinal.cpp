c769b0a056f397ceb945daffded688e85b767cb5
ving1981 <ving_tallica81@hotmail.com>
2021-07-16 13:07:45 -0500

    Caseta de vigilancia

--- ProyectoFinalTeoria_CruzPlata_MarinJimenez/ProyectoFinal.cpp
+++ ProyectoFinalTeoria_CruzPlata_MarinJimenez/ProyectoFinal.cpp
@@ -251,6 +251,7 @@ int main()
 	Model paredes_M("resources/objects/Paredes/paredes.obj");
 	Model allbotebasura_M("resources/objects/mobiliario/all_botesbasura.obj");
 	Model allbancas_M("resources/objects/Bancas/all_bancas.obj");
+	Model caseta_M("resources/objects/caseta/caseta.obj");
 	//Model arbol1_M("resources/objects/plantas/Tree_OBJ.obj");
 
 	// draw in wireframe
@@ -408,6 +409,12 @@ int main()
 		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		staticShader.setMat4("model", model);
 		allbancas_M.Draw(staticShader);
+
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.2f));
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		staticShader.setMat4("model", model);
+		caseta_M.Draw(staticShader);
 		// -------------------------------------------------------------------------------------------------------------------------
 		// Caja Transparente --- Siguiente Pr�ctica
 		// -------------------------------------------------------------------------------------------------------------------------
