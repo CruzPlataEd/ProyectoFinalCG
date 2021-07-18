f1122a740e731f1b4a21366a7895af2dbf267f4e
ving1981 <ving_tallica81@hotmail.com>
2021-07-17 23:07:46 -0500

    Restaurante

--- ProyectoFinalTeoria_CruzPlata_MarinJimenez/ProyectoFinal.cpp
+++ ProyectoFinalTeoria_CruzPlata_MarinJimenez/ProyectoFinal.cpp
@@ -255,6 +255,7 @@ int main()
 	Model casa_M("resources/objects/casa/all_casa.obj");
 	Model casa("resources/objects/mansion/new_house.obj");
 	Model tienda_M("resources/objects/tienda/tienda.obj");
+	Model restaurante_M("resources/objects/restaurante/comida.obj");
 	Model lampara_M("resources/objects/poste/all_lamparas.obj");
 	//Model arbol1_M("resources/objects/plantas/Tree_OBJ.obj");
 
@@ -438,6 +439,12 @@ int main()
 		staticShader.setMat4("model", model);
 		tienda_M.Draw(staticShader);
 
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.2f));
+		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		staticShader.setMat4("model", model);
+		restaurante_M.Draw(staticShader);
+		
 		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.2f));
 		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
